a060a08a56917c02c285449f6738d458f6f2a275
Malo Le Corvec <malolc2001@gmail.com>
2023-06-09 08:49:27 +0200

    fix ui login et homepage (#3)

--- app/src/pages/home/HomePage.jsx
+++ app/src/pages/home/HomePage.jsx
@@ -3,13 +3,12 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Home page content</p>
+      <h1>Page d&apos;acceuil</h1>
       <div>
-      <Link to="/login">Login</Link>
+        <Link to="/login">Login</Link>
       </div>
       <div>
-      <Link to="/dashboard">Dashboard</Link>
+        <Link to="/dashboard">Dashboard</Link>
       </div>
     </div>
   );
