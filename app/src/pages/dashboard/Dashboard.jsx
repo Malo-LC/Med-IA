c5592810cfa5af888a5a9eeca3d8fe890db1d181
Malo <malolc2001@gmail.com>
2023-06-30 15:20:08 +0200

    Dashboard UI

--- app/src/pages/dashboard/Dashboard.jsx
+++ app/src/pages/dashboard/Dashboard.jsx
@@ -4,7 +4,6 @@ import { toast } from "react-hot-toast";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import moment from "moment";
-moment.locale("fr");
 
 function Dashboard() {
   const [history, setHistory] = useState([]);
@@ -18,48 +17,43 @@ function Dashboard() {
     const response = await api.get("/analysis/history");
     if (!response?.ok) return toast.error(response.error || "Error while getting history");
     setHistory(response.data);
-    console.log(response.data);
   };
 
   return (
-    <div className=" h-screen flex flex-row">
-      <div className="flex flex-col w-full h-full ">
-        <h1 className="ml-10 text-4xl font-bold">Welcome {user.firstName} </h1>
-        <div className="bg-[#D9D9D9] w-2/3 rounded-md ml-10 mt-10">
-          <h1 className="ml-2 mt-2 font-bold">History</h1>
-          <div className="flex flex-row w-fit">
-            {history.map((item) => (
-              <div className="flex flex-col items-center justify-start w-fit" key={item.id}>
-                <p className="ml-2 mr-2 capitalize">{item.type}</p>
-                <img src={item.image} alt="image" className="w-1/4 " />
-                <p className="ml-2 mr-2">{moment(item.date).fromNow()}</p>
-                <p className="ml-2 mr-2">{item.result ? "Positive" : "Negative"}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-row">
-          <div className="bg-[#D9D9D9] w-2/5 h-5/6 rounded-md ml-10 mt-10 justify-center items-center">
-            <h1 className="ml-2 mt-2 font-bold">Last features used</h1>
-            <Link to="/dashboard/pneumonia" className="bg-[#4c4b4b] font-bold rounded-lg justify-center items-center hover:text-white">
-              <p className="ml-2 mr-2">Pneumonia Detection</p>
-            </Link>
-          </div>
-          <div className="bg-[#D9D9D9] w-2/5 h-5/6 rounded-md ml-10 mt-10 justify-center items-center truncate">
-            <div className="ml-5">
-              <h1 className="mt-2 font-bold">Agenda</h1>
-
-              <ul className="font-bold">Today - {moment().format("dddd Do MMM")}</ul>
-              <li>12h : RDV Madame Michu</li>
-              <li>15h : RDV Monsieur Georges</li>
-              <li>16h : RDV Madame Thomas</li>
-              <ul className="font-bold">Tomorrow - {moment().add(1, "day").format("dddd Do MMM")}</ul>
-              <li>Aucun événement à venir</li>
+    <div className="flex flex-col w-full h-full p-8 pt-4">
+      <h1 className="text-4xl font-bold">Welcome Dr {user.firstName} </h1>
+      <div className="border-y-2 border-[#080957] my-4 w-1/4" />
+      <div className="bg-slate-200 w-full rounded-md p-5">
+        <h1 className="font-bold mb-4">History</h1>
+        <div className="flex flex-row gap-3">
+          {history.map((item) => (
+            <div className="flex flex-col justify-center items-center bg-slate-300 rounded overflow-hidden w-[15vw]" key={item.id}>
+              <img src={item.image} alt="image" className="w-full " />
+              <div className="">{moment(item.date).fromNow()}</div>
+              <div className="">{item.Patient.first_name}</div>
+              <div className={`rounded-full p-2 mb-2 ${item.result ? "bg-red-500" : "bg-green-500"}`}>{item.result ? "Positive" : "Negative"}</div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
-      <div className="flex justify-center"></div>
+      <div className="flex flex-row mt-8 gap-8">
+        <div className="bg-slate-200 w-2/5 rounded-md justify-center items-center p-5">
+          <h1 className="font-bold mb-4">Last features used</h1>
+          <Link to="/dashboard/pneumonia" className="bg-slate-400 font-bold rounded-lg p-3 hover:text-white">
+            Pneumonia Detection
+          </Link>
+        </div>
+        <div className="bg-slate-200 w-2/5 rounded-md justify-center items-center p-5">
+          <h1 className="font-bold">Agenda</h1>
+
+          <ul className="font-bold">Today - {moment().format("dddd Do MMM")}</ul>
+          <li>12h : RDV Madame Mich</li>
+          <li>15h : RDV Monsieur Georges</li>
+          <li>16h : RDV Madame Thomas</li>
+          <ul className="font-bold">Tomorrow - {moment().add(1, "day").format("dddd Do MMM")}</ul>
+          <li>Aucun événement à venir</li>
+        </div>
+      </div>
     </div>
   );
 }
